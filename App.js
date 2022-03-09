2edc48f9bd33e9be4cb641ed4fc3d89985de4be4
maiconhenriquefa <maiconhenriquefa@gmail.com>
2022-03-09 20:29:58 -0300

    testando commit

--- App.js
+++ App.js
@@ -80,9 +80,7 @@ const App: () => Node = () => {
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
+          <Section title="Learn More">Testando Git</Section>
           <LearnMoreLinks />
         </View>
       </ScrollView>
