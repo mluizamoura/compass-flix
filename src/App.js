55d9855f133838712201a1695cc4466e8fa4fceb
maiconhenriquefa <maiconhenriquefa@gmail.com>
2022-03-10 12:32:53 -0300

    organizando pastas

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Compass Flix</Text>
+    </View>
+  );
+}
+
+export default App;
