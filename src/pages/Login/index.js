c5faad62e3542917813164225182acde73fe1f51
maiconhenriquefa <maiconhenriquefa@gmail.com>
2022-03-11 11:47:08 -0300

    estruturando a page para simular autenticaçao

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,19 @@
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import React from 'react';
+import styles from './styles';
 
 export default function Login() {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.background}>
+      <TextInput style={styles.input} placeholder={'e-mail'} />
+      <TextInput
+        style={styles.input}
+        placeholder={'senha'}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#000',
+    flex: 1,
+  },
+  input: {
+    backgroundColor: '#fff',
+  },
+  button: {
+    backgroundColor: 'blue',
+  },
+});
+
+export default styles;
