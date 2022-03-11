5afe68a4b2204baf6e1549fba6c87ca76d5883f3
maiconhenriquefa <maiconhenriquefa@gmail.com>
2022-03-11 13:30:46 -0300

    pegando o tolken gerado

--- src/service/api.js
+++ src/service/api.js
@@ -15,4 +15,15 @@ export async function getMovie() {
   }
 }
 
+export async function getTolken() {
+  try {
+    const {data} = await api.get(
+      'authentication/token/new?api_key=c3dc5cb91b1c309207a60a76c5742842',
+    );
+    return data.token;
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
 export default api;
