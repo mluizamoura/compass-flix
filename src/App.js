1af1698882592d24816038c66a15632150b3f993
Feliciano Brancarto <feliciano.brancarto@gmail.com>
2022-03-11 10:11:47 -0300

    Header style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <View>
-      <Text>Compass Flix</Text>
-    </View>
+    <Home />
+     
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,62 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function Home() {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={stylePageHome.container}>
+      <View style={stylePageHome.boxHeader}>
+        <Text style={stylePageHome.greetingText}>
+          Olá, <Text style={{color: '#E9A6A6'}}>John!</Text>
+        </Text>
+        <Text style={stylePageHome.textDescription}>
+          Reveja ou acompanhe os filmes que você assistiu...
+        </Text>
+        <Text style={stylePageHome.textPopularMovies}>
+          Filmes populares este mês
+        </Text>
+      </View>
+      <View style={stylePageHome.boxFlatList}>
+
+      </View>
     </View>
   );
 }
+
+const stylePageHome = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  boxHeader:{
+    height: "25%",
+
+  },
+  boxFlatList:{
+    height:"75%"
+
+  },
+
+  greetingText: {
+    color: '#FFFFFF',
+    marginTop: '15%',
+    marginLeft: '5%',
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontFamily:"open sans",
+    
+  },
+  textDescription: {
+    color: '#FFFFFF',
+    fontSize: 10,
+    marginLeft: '5%',
+    fontFamily:"open sans",
+  },
+
+  textPopularMovies: {
+    color: '#FFFFFF',
+    fontSize: 12,
+    marginTop: '7%',
+    marginLeft: '5%',
+    fontFamily:"open sans",
+  },
+});
