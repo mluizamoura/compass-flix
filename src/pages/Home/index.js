3b757d4ec9f74130ae6a5062e674ee200724745d
Kleber <kleberandrade1998@hotmail.com>
2022-03-11 21:55:17 -0300

    Salvando alterações

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,10 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {View, Text, FlatList} from 'react-native';
 import ActivityIndicatorViewNativeComponent from 'react-native/Libraries/Components/ActivityIndicator/ActivityIndicatorViewNativeComponent';
-// import api, {getMovie} from '../../service/api';
+import api, {getMovie, geet} from '../../service/api';
 
 export default function Home() {
-  // const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState(null);
 
   // useEffect(() => {
   //   async function awaitMovie() {
@@ -16,50 +16,58 @@ export default function Home() {
   // }, []);
 
 
+  // async function geet(){
+  //   const [data1,setData1] = useState([]);
+  //   const [loading,setLoading] = useState(false);
+  //   const [page,setPage] = useState(1);
+  //   try{
+  //     if(loading)return;
+  //     const {data} = await axios.get(`${baseURL}movie/popular?api_key=c3dc5cb91b1c309207a60a76c5742842&language=pt-BR&page=${page}`)
+  //     setData1([...data1, ...data.results]) 
+  //     setPage(page + 1)
+  //     setLoading(false)
+
+  // }
+  //   catch(error) {
+  //     consol1e.warn(error)
+  //   }
+  // }
+
+
   const baseURL= 'https://api.themoviedb.org/3/';
   const perPage = 20;
 
-  const [data1,setData1] = useState([]);
-  const [loading,setLoading] = useState(false);
-  const [page,setPage] = useState(1);
+
 
   useEffect(()=>{ 
-   geet()
+    async function awaitMovie(){
+      const response = geet()
+      setMovie(response)
+    }
+    awaitMovie()
    },[])
 
-   async function geet(){
-    try{
-      if(loading)return;
-      const {data} = await axios.get(`${baseURL}movie/popular?api_key=c3dc5cb91b1c309207a60a76c5742842&language=pt-BR&page=${page}`)
-      setData1([...data1, ...data.results]) 
-      setPage(page + 1)
-      setLoading(false)
-
-  }
-    catch(error) {
-      consol1e.warn(error)
-    }
-  }
+ 
 
 
 
 
 
 
-  console.warn(data1)
+  console.warn()
 
   return(
    <View>
-     <FlatList 
+     {/* <FlatList 
      data={data1}
      keyExtractor={item => item.id.toString()}
-     onEndReached={geet}
+     onEndReached={}
      onEndReachedThreshold={0.3}
      ListFooterComponent={ <FooterList Load={loading} /> }
      renderItem={({item}) => 
      <Text style={{fontSize:50, marginTop:120,backgroundColor:'pink'}}>{item.title}</Text>}
      
-     />
+     /> */}
   </View>
   );
 }
--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,8 @@
-
+import React,{useState} from 'react'
 import axios from 'axios'
 
 
-const Api = axios.create({
+const api = axios.create({
     baseURL: "https://api.themoviedb.org/3/"
 })
 
@@ -10,13 +10,30 @@ const Api = axios.create({
 
 export async function getMovie() {
     try{
-        const {data} = await Api.get('movie/popular?api_key=c3dc5cb91b1c309207a60a76c5742842&language=pt-BR&page=1')
+        const {data} = await api.get('movie/popular?api_key=c3dc5cb91b1c309207a60a76c5742842&language=pt-BR&page=1')
         return data.results
     }
       catch(error) {
         console.warn(error)
       }
   }
-  
 
-export default Api;
+  export async function geet(){
+    const [dados,setDados] = useState([]);
+    const [loading,setLoading] = useState(false);
+    const [page,setPage] = useState(1);
+    try{
+      if(loading)return;
+      const {data} = await api.get(`movie/popular?api_key=c3dc5cb91b1c309207a60a76c5742842&language=pt-BR&page=${page}`)
+      setData1([...dados, ...data.results]) 
+      setPage(page + 1)
+      setLoading(false)
+      return dados;
+
+  }
+    catch(error) {
+      consol1e.warn(error)
+    }
+  
+  }
+export default api;
