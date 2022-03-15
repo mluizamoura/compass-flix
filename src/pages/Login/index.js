ac53e58270e85ab2ec0316e9039c4f760c58fa5d
maiconhenriquefa <maiconhenriquefa@gmail.com>
2022-03-15 19:30:23 -0300

    correcting style and adding news properts at inputs

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -6,65 +6,56 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
-  TouchableWithoutFeedback,
 } from 'react-native';
 import styles from './styles';
-import Icon1 from 'react-native-vector-icons/EvilIcons';
-import Icon2 from 'react-native-vector-icons/AntDesign';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function Login() {
   return (
-    <View style={styles.container}>
-      <KeyboardAvoidingView>
-        <View>
-          <View>
-            <Image
-              style={styles.banner}
-              source={require('../../assets/Banner.png')}
-            />
-          </View>
-          <View style={styles.logo}>
-            <Image source={require('../../assets/login.png')} />
-          </View>
+    <KeyboardAvoidingView behavior="position" style={styles.container}>
+      <View>
+        <Image
+          style={styles.banner}
+          source={require('../../assets/bannerLogin.png')}
+        />
+        <View style={styles.logo}>
+          <Image source={require('../../assets/logo.png')} />
         </View>
-        <View style={styles.textEntry}>
-          <Text style={{fontWeight: 'bold', fontSize: 24, color: '#fff'}}>
-            Login
+      </View>
+      <View style={styles.textEntry}>
+        <Text style={styles.textEntry.login}>Login</Text>
+        <Text style={styles.textEntry.description}>
+          Entre na sua conta para continuar.
+        </Text>
+      </View>
+      <View style={styles.inputArea}>
+        <EvilIcons style={styles.userIcon} name="user" color="#fff" size={20} />
+        <TextInput
+          keyboardType={'email-address'}
+          autoComplete={'email'}
+          style={styles.input}
+          placeholder="e-mail"
+          onChangeText={() => {}}
+          placeholderTextColor="rgba(255, 255, 255, 0.5)"
+        />
+        <AntDesign style={styles.lockIcon} name="lock" color="#fff" size={18} />
+        <TextInput
+          autoComplete={'password'}
+          style={styles.input}
+          placeholder="senha"
+          onChangeText={() => {}}
+          placeholderTextColor="rgba(255, 255, 255, 0.5)"
+          secureTextEntry={true}
+        />
+      </View>
+      <View style={styles.btnSubmit}>
+        <TouchableOpacity>
+          <Text style={{fontSize: 14, fontWeight: 'bold', color: '#1F1D36'}}>
+            Entrar
           </Text>
-          <Text style={{fontSize: 13, color: '#fff'}}>
-            Entre na sua conta para continuar.
-          </Text>
-        </View>
-
-        <View style={styles.inputArea}>
-          <Icon1 style={styles.userIcon} name="user" color="#fff" size={20}/> 
-          <TextInput 
-            style={styles.input}
-            placeholder="e-mail"
-            onChangeText={() => {}}
-            placeholderTextColor="rgba(255, 255, 255, 0.5)"
-            />
-          
-          
-          <Icon2 style={styles.lockIcon} name="lock" color="#fff" size={18}/> 
-          <TextInput 
-            style={styles.input}
-            placeholder="senha"
-            onChangeText={() => {}}
-            placeholderTextColor="rgba(255, 255, 255, 0.5)"
-            secureTextEntry={true}
-          />
-         
-        </View>
-
-        <View style={styles.btnSubmit}>
-          <TouchableOpacity>
-            <Text style={{fontSize: 14, fontWeight: 'bold', color: '#1F1D36'}}>
-              Entrar
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </KeyboardAvoidingView>
-    </View>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -4,47 +4,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-    
   },
   inputArea: {
     borderRadius: 35,
     marginBottom: 20,
     alignItems: 'center',
-    
-    
-  },
-  btnSubmit: {
-    backgroundColor: '#E9A6A6',
-    borderRadius: 35,
-    alignItems: 'center',
-    height: 35,
-    width: 95,
-    justifyContent: 'center',
-    alignSelf: 'center',
   },
   banner: {
-    resizeMode:"cover",
-    
-    
+    resizeMode: 'contain',
+    width: '100%',
   },
-
   textEntry: {
     alignItems: 'center',
-    
-    
+    login: {
+      fontWeight: 'bold',
+      fontSize: 24,
+      color: '#fff',
+    },
+    description: {
+      fontSize: 13,
+      color: '#fff',
+    },
   },
-
   logo: {
     width: '100%',
     alignItems: 'center',
     position: 'absolute',
     bottom: -50,
     marginHorizontal: 0,
-    
   },
-
   input: {
-    
+    color: '#fff',
     backgroundColor: '#C4C4C459',
     borderRadius: 30,
     width: 243,
@@ -53,8 +43,6 @@ const styles = StyleSheet.create({
     paddingLeft: 40,
     paddingBottom: 8,
     fontSize: 9,
-    
-
   },
   userIcon: {
     padding: 10,
@@ -64,6 +52,16 @@ const styles = StyleSheet.create({
   lockIcon: {
     right: 100,
     top: 25,
+  },
+  btnSubmit: {
+    backgroundColor: '#E9A6A6',
+    borderRadius: 35,
+    alignItems: 'center',
+    height: 35,
+    width: 95,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    marginBottom: 20,
   },
 });
 
