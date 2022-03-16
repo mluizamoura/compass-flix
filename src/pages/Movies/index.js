be90029ad3839fa56463d708d19473cc003fdf35
Soufelipeee <98565012+Soufelipeee@users.noreply.github.com>
2022-03-16 19:38:06 -0300

    Styling page home

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -3,17 +3,17 @@ import {
   Text,
   TouchableOpacity,
   Image,
-  StyleSheet,
   FlatList,
 } from 'react-native';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Feather from 'react-native-vector-icons/Feather';
-import {NavigationHelpersContext} from '@react-navigation/native';
+import { NavigationHelpersContext } from '@react-navigation/native';
 import api from '../../service/api';
+import styles from './styles'
 
-export default function Movies({route, navigation}) {
-  const {item} = route.params;
+export default function Movies({ route, navigation }) {
+  const { item } = route.params;
   const testflatlist = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const [details, setDetails] = useState([]);
@@ -23,7 +23,7 @@ export default function Movies({route, navigation}) {
   useEffect(() => {
     async function getDetails() {
       try {
-        const {data} = await api.get(
+        const { data } = await api.get(
           `https://api.themoviedb.org/3/movie/${id}?api_key=c3dc5cb91b1c309207a60a76c5742842&language=pt-BR`,
         );
         setDetails(data);
@@ -37,7 +37,7 @@ export default function Movies({route, navigation}) {
   useEffect(() => {
     async function getElenco() {
       try {
-        const {data} = await api.get(
+        const { data } = await api.get(
           `https://api.themoviedb.org/3/movie/${id}/credits?api_key=c3dc5cb91b1c309207a60a76c5742842&language=pt-BR`,
         );
         setElenco(data.cast);
@@ -66,34 +66,56 @@ export default function Movies({route, navigation}) {
         <Feather name="star" size={25} style={styles.buttonStar} />
       </TouchableOpacity>
 
-      <Image
-        style={styles.capaMovie}
-        source={{
-          uri: `http://image.tmdb.org/t/p/w780/${details.poster_path}`,
-        }}
-      />
+      <View style={styles.detailsMovies}>
 
-      <Text style={styles.titleMovie}>{details.original_title}</Text>
-      <Text style={styles.yearMovie}>{new Date(details.release_date).getFullYear()}</Text>
-      <Text style={styles.timeMovie}>{details.runtime}min</Text>
-      <Text style={styles.autorMovie}>Direção por Matt Reeves</Text>
-      <Text style={styles.ratedMovie}>{details.vote_average}/10</Text>
-      <TouchableOpacity>
-        <AntDesign name="heart" size={22} style={styles.heartIcon} />
-      </TouchableOpacity>
-      <Text style={styles.liked}>{details.popularity}K</Text>
+        <View>
+          <Image
+            style={styles.capaMovie}
+            source={{
+              uri: `http://image.tmdb.org/t/p/w780/${details.poster_path}`,
+            }}
+          />
+        </View>
 
-      <Text style={styles.descriptionMovie}>
-        {details.overview}
-      </Text>
-      <Text style={styles.elenco}>Elenco</Text>
+
+        <View style={styles.detaisMoviesTitle}>
+          <Text style={styles.titleMovie}>{details.title} <Text style={styles.yearMovie}>{new Date(details.release_date).getFullYear()}</Text> <Text style={styles.timeMovie}>{details.runtime}min</Text>
+          </Text>
+          <View>
+            <Text style={styles.textAutor}>Direção por <Text style={styles.autorMovie}>Dirigido por......</Text>
+            </Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View style={styles.datailRatedLiked}>
+
+        <View style={styles.detailsRated}>
+          <Text style={styles.ratedMovie}>{details.vote_average}/10</Text>
+        </View>
+
+        <View style={styles.datailsLiked}>
+          <TouchableOpacity>
+            <AntDesign name="heart" size={20} style={styles.heartIcon} />
+          </TouchableOpacity>
+          <Text style={styles.liked}>{details.popularity}K</Text>
+        </View>
+
+      </View>
+    
+        <Text style={styles.descriptionMovie}>
+          {details.overview}
+        </Text>
+        <Text style={styles.elenco}>Elenco</Text>
+     
+
 
       <FlatList
         style={styles.viewFLatList}
         data={elenco}
         keyExtractor={(item, index) => index}
-        contentContainerStyle={{alignItems: 'center'}}
-        renderItem={({item}) => (
+        renderItem={({ item }) => (
           <View>
             <Image
               style={styles.imageFlatList}
@@ -106,175 +128,8 @@ export default function Movies({route, navigation}) {
           </View>
         )}
       />
-    </View>
+    </View >
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-  },
-
-  backGroundMovie: {
-    width: 420,
-    height: 170.42,
-    backgroundColor: 'pink',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  containerButtonBack: {
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center',
-    top: 19,
-    left: 20,
-    backgroundColor: 'white',
-    borderRadius: 40,
-    width: 30,
-    height: 30,
-  },
-
-  buttonBack: {
-    color: 'black',
-  },
-
-  containerButtonStar: {
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center',
-    top: 19,
-    right: 20,
-    backgroundColor: 'white',
-    borderRadius: 40,
-    width: 30,
-    height: 30,
-  },
-  buttonStar: {
-    color: 'black',
-  },
-
-  capaMovie: {
-    zIndex: 999,
-    position: 'relative',
-    left: 20,
-    top: -45,
-    width: 116,
-    height: 182,
-    alignSelf: 'flex-start',
-    borderRadius: 8,
-  },
-
-  titleMovie: {
-    color: '#FFFFFF',
-    fontSize: 20,
-    fontWeight: 'bold',
-    fontFamily: 'Open Sans',
-    marginLeft: 70,
-    marginTop: -160,
-  },
-
-  yearMovie: {
-    fontSize: 10,
-    fontWeight: 'bold',
-    fontFamily: 'Open Sans',
-    alignSelf: 'flex-end',
-    marginTop: -17,
-    marginRight: 60,
-    color: '#FFFFFF',
-  },
-
-  timeMovie: {
-    fontSize: 7,
-    color: 'white',
-    alignSelf: 'flex-end',
-    marginRight: 25,
-  },
-
-  autorMovie: {
-    alignSelf: 'center',
-    marginLeft: 70,
-    fontSize: 10,
-    color: '#FFFFFF',
-  },
-
-  ratedMovie: {
-    color: '#E9A6A6',
-    fontSize: 30,
-    marginTop: 25,
-  },
-
-  heartIcon: {
-    color: 'red',
-    alignSelf: 'center',
-    marginLeft: 200,
-    marginTop: -30,
-  },
-
-  liked: {
-    marginTop: -5,
-    marginLeft: 202,
-    color: 'white',
-    textAlign: 'center',
-    fontFamily: 'Open Sans',
-    fontSize: 10,
-  },
-
-  descriptionMovie: {
-    color: '#FFFFFF',
-    fontSize: 12,
-    width: 332,
-    height: 100,
-    alignSelf: 'flex-start',
-    marginTop: 30,
-    marginLeft: 20,
-  },
-
-  elenco: {
-    paddingVertical: 4,
-    textAlign: 'center',
-    marginTop: -25,
-    borderColor: 'white',
-    borderWidth: 1,
-    borderRadius: 12,
-    marginLeft: 20,
-    color: 'white',
-    alignSelf: 'flex-start',
-    width: 40,
-
-    backgroundColor: '#9C4A8B',
-    fontSize: 10,
-    marginBottom: 5,
-  },
-
-  viewFLatList: {
-    flex: 1,
-    backgroundColor: 'black',
-    width: 400,
-    flexWrap: 'wrap',
-  },
-
-  imageFlatList: {
-    marginLeft: 20,
-    marginTop: 10,
-    width: 35,
-    height: 35,
-    borderRadius: 50,
-  },
-
-  name: {
-    color: 'white',
-    fontSize: 15,
-    marginLeft: 70,
-    marginTop: -40,
-  },
-
-  character: {
-    color: 'white',
-    fontSize: 11,
-    marginLeft: 70,
-  },
-});
